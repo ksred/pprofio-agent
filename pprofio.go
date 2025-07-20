2d0234659ecd3451911206e026ff02b0e99ed433
ksred <git@ksred.me>
2025-07-20 14:45:24 +0100

    chore: bump version to v0.1.3

--- pprofio.go
+++ pprofio.go
@@ -13,7 +13,7 @@ import (
 )
 
 // Version is the current package version
-const Version = "0.1.2"
+const Version = "0.1.3"
 
 // applyDefaults applies default values to the configuration
 func applyDefaults(config *Config) {
